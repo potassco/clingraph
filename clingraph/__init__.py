5276afe7d7d2d9e7859ed589050db79b28040e78
Susana Hahn <susuhahnml@yahoo.com.mx>
2021-12-13 20:19:05 -0600

    Added features for multigraph and subgraphs

--- clingraph/__init__.py
+++ clingraph/__init__.py
@@ -1,7 +1,12 @@
 from clingo.application import Application, clingo_main, Flag
-from clingo.symbol import Function
+from clingo.symbol import Function, String
+from clingo.script import enable_python
+enable_python()
 from graphviz import Graph, Digraph
 from collections import defaultdict
+import networkx as nx
+import copy
+import os
 
 class Clingraph(Application):
     program_name = 'clingraph'
@@ -13,38 +18,55 @@ class Clingraph(Application):
     view = Flag(False)
 
     directory = 'out'
-    format = None
-    engine = None
+    format = 'pdf'
+    engine = 'dot'
 
     prefix = ''
+
+    graphs = []
+
 
     def main(self, ctl, files):
         for path in files:
             ctl.load(path)
         if not files:
             ctl.load('-')
-        ctl.ground([('base', [])], context=self)
-        ctl.solve()
+        ctl.ground([('base', [])])
+        ctl.solve(on_model=self.model2graphs)
 
-    def parse_directory(self, dir):
+        #Save graphs
+        for graph in self.graphs:
+            if self.render or self.view:
+                path = graph.render(
+                    view=self.view,
+                    cleanup=True,
+                    quiet_view=self.view,
+                )
+            else:
+                f = open(graph.filepath, "a")
+                f.write(graph.source)
+                f.close()
+                
+    def _parse_directory(self, dir):
         self.directory = dir
-        #TODO check that it is actually a directory
         return True
     
-    def parse_format(self, format):
+    def _parse_format(self, format):
+        if not format:
+            format = 'png'
         self.format = format
+
         return format in ['pdf','svg','png']
 
-    def parse_engine(self, engine):
+    def _parse_engine(self, engine):
         self.engine = engine
         return engine in ["dot", "neato", "twopi", "circo", "fdp", "osage", "patchwork", "sfdp"]
 
-    def parse_prefix(self, prefix):
+    def _parse_prefix(self, prefix):
         self.prefix = prefix
         return not " " in prefix
 
     def register_options(self, options):
-        # TODO add default
         options.add_flag(self.option_group, 'render', 
             'Render the answers', self.render)
         options.add_flag(self.option_group, 'view', 
@@ -59,40 +81,54 @@ class Clingraph(Application):
             'Prefix used for the predicates that the package will consider', self._parse_prefix, argument='<engine>' )
 
     def model2graphs(self, model):
-        elements = ['graph','cluster','class','node','edge']
-        basic_info = {'graph': 'default','cluster': None, 'attr': {}}
+        # Define structure
+        elements = ['graph','class','node','edge']
+        basic_info = {'graph': 'default', 'attr': {}}
+        graph_info = {'graph':None,'attr': {'type':'graph'}}
         structure = {
-            'graph': defaultdict(lambda: {'attr': [('type','graph')]}.copy()),
-            'cluster': defaultdict(lambda: basic_info.copy()),
-            'class': defaultdict(lambda: basic_info.copy()),
-            'node': defaultdict(lambda: basic_info.copy()),
-            'edge': defaultdict(lambda: basic_info.copy())
+            'graph': defaultdict(lambda: copy.deepcopy(graph_info)),
+            'class': defaultdict(lambda: copy.deepcopy(basic_info)),
+            'node': defaultdict(lambda: copy.deepcopy(basic_info)),
+            'edge': defaultdict(lambda: copy.deepcopy(basic_info))
         }
         structure['graph']['default']
         structure['class']['node']
         structure['class']['edge']
 
-        for atom in model.symbols(atoms=True):
-            if not atom.startswith(self.prefix):
-                continue
+        # Fill structure with model
+        def s2id(symbol,type=''):
+            if type == 'edge':
+                if len(symbol.arguments)!=2:
+                    #TODO handle errors better
+                    return ValueError
+                return tuple([str(a).strip('"') for a in symbol.arguments])
+            else:
+                return str(symbol).strip('"')
+
+
+        for atom in model.symbols(atoms=True,shown=True):
             name = atom.name
+            if not name.startswith(self.prefix):
+                continue
             if atom.match(self.prefix+"attr", 4):
                 #TODO maybe nicer way to treat integers
-                element_type = str(atom[0]).strip('"')
-                element_id = str(atom[1]).strip('"')
-                attr_name = str(atom[2]).strip('"')
-                attr_value = str(atom[3]).strip('"')
-                if attr_name in ['graph','cluster']:
-                    structure[attr_name][attr_value]
-                    structure[element_type][element_id][attr_name]=attr_value
-                else:
-                    structure[element_type][element_id]['attr'][attr_name]=attr_value
-                continue
+                args = atom.arguments
+                element_type = s2id(args[0])
+                element_id = s2id(args[1],element_type)
+                attr_name = s2id(args[2])
+                attr_value = s2id(args[3])
+                structure[element_type][element_id]['attr'][attr_name]=attr_value
             for e in elements:
                 if atom.match(self.prefix+e,1):
-                    structure[e]
+                    structure[e][s2id(atom.arguments[0],e)]
+                    continue
+                if atom.match(self.prefix+e,2):
+                    structure[e][s2id(atom.arguments[0],e)]['graph']=s2id(atom.arguments[1])
                     continue
 
+        # Create graphs
+        graphs = {}
+        dir = os.path.join(self.directory,f'model_{model.number:04d}')
         for g_id, g in structure['graph'].items():
             if g['attr']['type'] == 'graph':
                 _Graph = Graph
@@ -101,80 +137,69 @@ class Clingraph(Application):
             else: raise ValueError
 
             graph_name = g['attr']['name'] if 'name' in  g['attr'] else g_id
+            #TODO new directory per stable model as an option
+            node_attr = structure['class']['node']['attr'] if g['graph'] is None else {}
+            edge_attr = structure['class']['edge']['attr'] if g['graph'] is None else {}
             graph = _Graph(
-                name = f'{graph_name}_{model.number:04d}',
-                directory = self.directory,
+                name = f'{graph_name}',
+                directory = dir,
                 format = self.format,
                 engine = self.engine,
                 graph_attr = g['attr'],
-                node_attr = structure['class']['node'],
-                edge_attr = structure['class']['edge']
+                node_attr = node_attr,
+                edge_attr = edge_attr
             )
+            graphs[g_id]=graph
+
+        # Add elements
+        single_element = ['node', 'edge']
+        default_used = False
+        for e in single_element:
+            for id, vals in structure[e].items():
+                g = graphs[vals['graph']]
+                default_used = default_used or vals['graph']=='default'
+                if 'class' in vals['attr']:
+                    class_attr = structure['class'][vals['attr']['class']]['attr'].copy()
+                else:
+                    class_attr = {}
+                if e == 'node':
+                    g.node(id, **({**class_attr, **vals['attr']}))
+                else:
+                    g.edge(id[0], id[1], **({**class_attr, **vals['attr']}))
+
+        if not default_used:
+            del structure['graph']['default']
         
+        
+        # Make subgraphs
+        graph_order = [(g_id, g['graph'] or 'top')
+                      for g_id, g in structure['graph'].items()]
+        nx_g = nx.DiGraph(graph_order)
+        final_order = list(nx.topological_sort(nx_g))
+
+        final_graphs = []
+        for g_id in final_order:
+            if g_id == 'top':
+                continue
+
+            g_info = structure['graph'][g_id]
+            g_parent = g_info['graph']
+            g = graphs[g_id]
+            if g_parent is None:
+                final_graphs.append(g)
+                continue
+            g.name = 'cluster_'+g.name
+            graphs[g_parent]
+            graphs[g_parent].subgraph(g)  
+        
+        self.graphs = self.graphs + final_graphs
 
     def print_model(self, model, printer):
-        types = []
-        nodes = []
-        edges = []
-        attrs = { 'graph': {}, 'node': {}, 'edge': {} }
-
-        for atom in model.symbols(shown=True):
-            if atom.match(self.type, 1):
-                types.append(str(atom.arguments[0]))
-
-            elif atom.name == self.node and len(atom.arguments) in [1, 2]:
-                nodes.append(tuple(map(str, atom.arguments)))
-
-            elif atom.name == self.edge and len(atom.arguments) in [2, 3]:
-                edges.append(tuple(map(str, atom.arguments)))
-
-            elif atom.match(self.attr, 3):
-                entity = atom.arguments[0]
-                if entity.name in ['graph', 'node', 'edge'] and len(entity.arguments) == 0:
-                    entity = entity.name
-                elif entity.match('node', 1) or entity.match('edge', 2):
-                    entity = tuple(map(str, entity.arguments))
-                else: raise ValueError
-
-                key = str(atom.arguments[1])
-                val = str(atom.arguments[2])
-
-                if entity not in attrs:
-                    attrs[entity] = {}
-
-                attrs[entity][key] = val
-
-        if types in [[], ['graph']]:
-            _Graph = Graph
-        elif types in [['digraph']]:
-            _Graph = Digraph
-        else: raise ValueError
-
-        graph = _Graph(
-            name = f'{model.number:04d}',
-            directory = self.directory,
-            format = self.format,
-            engine = self.engine,
-            graph_attr = attrs['graph'],
-            node_attr = attrs['node'],
-            edge_attr = attrs['edge'],
-        )
-
-        for node in nodes:
-            graph.node(*node, _attributes=attrs.get(node[0:1]))
-
-        for edge in edges:
-            graph.edge(*edge, _attributes=attrs.get(edge[0:2]))
-
-        if self.render or self.view:
-            path = graph.render(
-                view=self.view,
-                cleanup=True,
-                quiet_view=self.view,
-            )
-            print(f'Saved to {path}')
-        else:
-            print(graph.source)
+        print(model)
+        
+        print("\n" + "-"*20 +' Clingraph ' + "-"*20 + "\n")
+        for graph in self.graphs:
+            print(f"Output saved in {graph.filepath}")
 
 def main():
     clingo_main(Clingraph())
