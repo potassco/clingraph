1ec65538108b0767b496705da44847f593a75a8f
Susana Hahn <susuhahnml@yahoo.com.mx>
2022-01-20 18:58:23 -0600

    Restructure of proyect
    - No longer a clingo application
    - Clingraph is now a package
    - The management of the fact database is outourced to an independet class
    - Implementation of an ORM that uses clorm
    - Jupyter notbook with usage example
    - Auxiliaty functions for Jupyter usage
    - Dynamic example for the elevator
    - Possible multiple attributes with the same name
    	- As a set
    	- With a specific position
    
    Pending work for the update:
    - Reimplementation of the console functionality
    - Edit the README
    - Edit README for examples

--- clingraph/__main__.py
+++
@@ -1,7 +0,0 @@
-from . import main
-
-if __name__ == "__main__":
-    """
-    Runs the main function
-    """
-    main()
---
+++ clingraph/orm.py
@@ -0,0 +1,82 @@
+class ClingraphORM():
+    """
+    An interface for an ORM used in clingraph
+    """
+
+    def __init__(self, prefix: str = ""):
+        """
+        Creates an ORM that will serve as a database for the facts
+        Arguments:
+            prefix (str): The prefix to all predicate names
+        """
+        pass
+
+    def add_fact_string(self, program):
+        """
+        Adds a string containing facts to the database
+        Arguments:
+            program (str): A string consisting of only facts, devided by a '.'
+        """
+        pass
+
+    def add_fact_file(self, file):
+        """
+        Adds a file containing facts to the database
+        Arguments:
+            file (str): The path to the file
+        """
+        pass
+
+    def add_clingo_model(self, model):
+        """
+        Adds a clingo model to the database
+        Arguments:
+            model (clingo.Model): A model retured by clingo
+        """
+        pass
+
+    def get_all_graphs(self):
+        """
+        Gets a list if the identifiers for all the graphs
+        """
+        pass
+
+    def parent_graph(self, graph_id):
+        """
+        Gets the parent graph for a given graph_id.
+        Returns None if there is not such supertype
+        Arguments:
+            graph_id: Identifier of the subgraph
+        """
+        pass
+
+    def get_graph_global_element_attr(self, element_type, graph_id):
+        """
+        Gets the attributes for a global element: graph_nodes or graph_edges.
+        Returns a dictionary where the keys are attribute name and values are
+        attribute values.
+        Arguments:
+            element_type (str): The element type: 'edge' or 'node'
+            graph_id: Identifier of the graph
+        """
+        pass
+
+    def get_graph_elements(self, element_type, graph_id):
+        """
+        Gets the list of elements for a graph
+        Arguments:
+            element_type (str): The element type: 'edge' or 'node'
+            graph_id: Identifier of the graph
+        """
+        pass
+
+    def get_element_attr(self, element_type, element_id):
+        """
+        Gets the attributes a specific element
+        Returns a dictionary where the keys are attribute name and values are
+        attribute values.
+        Arguments:
+            element_type (str): The element type: 'graph', 'edge' or 'node'
+            element_id: Identifier of the element
+        """
+        pass
