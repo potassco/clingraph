782cd9f32394192d1bb3533d4c6cbcb48645a57b
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2022-02-02 16:02:48 +0100

    extend ci tests

--- build/lib/clingraph/exception.py
+++
@@ -1,8 +0,0 @@
-import logging
-LOG = logging.getLogger('custom')
-
-class InvalidSyntax(Exception):
-    """Returned when the input syntax is not expected"""
-    def __init__(self, *args):
-        LOG.error("\n".join(args))
-        super().__init__("\n".join(args))
